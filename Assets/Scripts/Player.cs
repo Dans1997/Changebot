0972e14a034fce99d4d88d4a695868a47bbfa575
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-07-11 18:41:28 -0300

    2D Pixel Perfect Set Up & Player Jump

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 5f;
+    [SerializeField] float jumpForce = 5f;
+    [SerializeField] int maxExtraJumps = 2;
+    [SerializeField] float maxJumpTime = 1f;
+
+    [SerializeField] Transform groundCheck;
+    [SerializeField] float groundCheckRadius = 0f;
+    [SerializeField] LayerMask whatIsGround;
+
+    bool isGrounded = true;
+    bool isJumping = false;
+
+    int extraJumps;
+    float moveInput = 0f;
+    float jumpTimeCounter;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJumps = maxExtraJumps;
+        jumpTimeCounter = maxJumpTime;
+    }
+
+    // Physics Related Update
+    void FixedUpdate()
+    {
+        GroundCheck();
+        MovePlayer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleJump();
+    }
+
+    private void GroundCheck() 
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void MovePlayer()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        float speedX = moveInput * moveSpeed * Time.deltaTime;
+        GetComponent<Rigidbody2D>().velocity = new Vector2(speedX, GetComponent<Rigidbody2D>().velocity.y);
+    }
+
+    private void HandleJump()
+    {
+        if (isGrounded) extraJumps = maxExtraJumps;
+
+        Vector2 jumpVector = jumpForce * Vector2.up;
+
+        if (Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            isJumping = true;
+            jumpTimeCounter = maxJumpTime;
+            GetComponent<Rigidbody2D>().velocity = jumpVector;
+            extraJumps--;
+        } 
+        else if (Input.GetKeyDown(KeyCode.Space) && extraJumps == 0 && isGrounded)
+        {
+            isJumping = false;
+            jumpTimeCounter = maxJumpTime;
+            GetComponent<Rigidbody2D>().velocity = jumpVector;
+        }
+
+        // Holding Down Space
+        if(Input.GetKey(KeyCode.Space))
+        {
+            if(jumpTimeCounter > 0 && isJumping)
+            {
+                GetComponent<Rigidbody2D>().velocity = jumpVector;
+                jumpTimeCounter -= Time.deltaTime;
+            }
+            else
+            {
+                isJumping = false;
+            }
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space)) isJumping = false;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # Change
  A game jam from 2020-07-11 to 2020-07-26 hosted by J & lysander.
+
+# Project Set-Up
+ For each asset you’re using, select the asset in the Project view, and then change the following settings in the inspector:
+
+  - 32x32 Resolution (32PPU)
+  - Filter mode changed to "Point"
+  - Compression changed to "None"
+  - Pivot unit mode set to ‘Pixels’ instead of ‘Normalized’
+
