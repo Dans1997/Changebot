e8a7a47b3dd2927d9a5ee1fa2b860a535a1c4e95
kaikecarlos <caheasta@gmail.com>
2020-07-14 18:19:34 -0300

    Update PlayerSize.cs

--- Assets/Scripts/Player/PlayerSize.cs
+++ Assets/Scripts/Player/PlayerSize.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,7 +18,9 @@ public class PlayerSize : MonoBehaviour
     {
         HandleSizeChange();
     }
-
+    // Tiny - 0.5
+    // Normal - 1
+    // Big - 2
     private void HandleSizeChange()
     {
         // Control Power Are Controlled By Keys (FOR NOW)
@@ -27,6 +29,7 @@ public class PlayerSize : MonoBehaviour
             if (playerSize.Equals(Size.Tiny)) { Debug.Log("Player is already tiny!"); return; }
             Debug.Log("Go Tiny!");
             playerSize = Size.Tiny;
+            transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
             return;
         }
         else if (Input.GetKeyDown(KeyCode.Y))
@@ -34,6 +37,7 @@ public class PlayerSize : MonoBehaviour
             if (playerSize.Equals(Size.Normal)) { Debug.Log("Player is already normal!"); return; }
             Debug.Log("Go Normal!");
             playerSize = Size.Normal;
+            transform.localScale = new Vector3(1f,1f,1f);
             return;
         }
         else if (Input.GetKeyDown(KeyCode.U))
@@ -41,6 +45,7 @@ public class PlayerSize : MonoBehaviour
             if (playerSize.Equals(Size.Big)) { Debug.Log("Player is already big!"); return; }
             Debug.Log("Go Big!");
             playerSize = Size.Big;
+            transform.localScale = new Vector3(2f,2f,2f);
             return;
         }
     }
