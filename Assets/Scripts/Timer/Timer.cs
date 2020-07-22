d3deae007faeb8972e79b9dce5934740e19462f7
Danillo Neves Souza <danillo.neves97@gmail.com>
2020-07-21 23:10:02 -0300

    Implemented Level Timer

---
+++ Assets/Scripts/Timer/Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    [Header("Timer UI")]
+    [SerializeField] Text timerText;
+
+    int timeElapsed = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CountTime());
+    }
+
+    IEnumerator CountTime()
+    {
+        while(true) 
+        {
+            timerText.text = timeElapsed.ToString();
+            yield return new WaitForSeconds(1f);
+            timeElapsed++;
+        }
+    }
+
+
+}
